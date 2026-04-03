5e4abca5b87bd1c394588303fc2df9223e06634b
Konstantin Mohin <cosmos@Konstantins-Mac-mini.local>
2026-04-03 17:22:54 +0300

    make ui load synchronously within the initial entry chunk

--- packages/client/webpack.config.cjs
+++ packages/client/webpack.config.cjs
@@ -57,7 +57,7 @@ module.exports = {
           eager: true,
           requiredVersion: '^7.13.0',
         },
-        ui: { singleton: true },
+        ui: { singleton: true, eager: true },
       },
     }),
     new HtmlWebpackPlugin({
